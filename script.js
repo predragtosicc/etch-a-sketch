5a17826f4e05e608745badb6a54213c9a662e0fe
Predrag Tosic <predrag.tosicc@gmail.com>
2023-05-17 23:14:25 +0200

    Created a function to draw divs, instead of it just being a loop.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 
 let mainContainer = document.querySelector('.main-container');
 
-
-// Loop for creating column, and row divs
-for (let i = 0; i < 16; i++){
-    let divColumn = document.createElement('div')
-    divColumn.classList.add('column-div');
-    mainContainer.appendChild(divColumn);
-    for (let j = 0; j < 16; j++){
-        let divRow = document.createElement('div');
-        divRow.classList.add('row-div');
-        divColumn.appendChild(divRow);
+function drawDiv(col = 16,row = 16){
+    // Loop for creating column, and row divs
+    for (let i = 0; i < 16; i++){
+        let divColumn = document.createElement('div')
+        divColumn.classList.add('column-div');
+        mainContainer.appendChild(divColumn);
+        for (let j = 0; j < 16; j++){
+            let divRow = document.createElement('div');
+            divRow.classList.add('row-div');
+            divColumn.appendChild(divRow);
+        }
     }
 }
 
+drawDiv()
+// Creating a array out of all the row divs
 const divs = document.querySelectorAll('.row-div');
 const divsArr = Array.from(divs);
 
+// Looping over row-diws and changing color upon hovering over them
 divsArr.forEach(div => {
     div.addEventListener('mouseover', () => {
         div.style.backgroundColor = 'black';
     })
-
 })
