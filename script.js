91d1306df32f96fd0d6f5f03db9b183d6e75e69d
Predrag Tosic <predrag.tosicc@gmail.com>
2023-05-21 00:14:15 +0200

    Edited the drawDiv function, so it resets the board first, then draws divs

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
-let mainContainer = document.querySelector('.main-container');
 
 function drawDiv(n){
+    // Checking for children, reseting the board
+    let mainContainer = document.querySelector('.main-container');
+    while(mainContainer.hasChildNodes()){
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
     // Loop for creating column, and row divs
     for (let i = 0; i < n; i++){
         let divColumn = document.createElement('div')
@@ -15,9 +19,14 @@ function drawDiv(n){
     }
 }
 
-
-
-
+const boardSize = document.querySelectorAll('.size');
+const boardSizeArr = Array.from(boardSize);
+boardSizeArr.forEach(size => {
+    size.addEventListener('click', () => {
+        console.log(size.value)  
+        drawDiv(size.value)     
+    })
+}); 
 // Creating a array out of all the row divs
 const divs = document.querySelectorAll('.row-div');
 const divsArr = Array.from(divs);
@@ -27,4 +36,4 @@ divsArr.forEach(div => {
     div.addEventListener('mouseover', () => {
         div.style.backgroundColor = 'black';
     })
-})
+});
